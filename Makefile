88e013116b8ce8bf37848170abdd46e5ebee659d
mikumaycry <me@wbuntu.com>
2021-02-07 11:12:25 +0800

    initial commit

---
+++ Makefile
@@ -0,0 +1,6 @@
+
+VERSION ?= latest
+
+image:
+	docker build -t docker.io/wbuntu/mtproxy:$(VERSION) .
+	@echo "build client image docker.io/wbuntu/mtproxy:$(VERSION)"
---
+++ mtproxy.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+
+secret_file=/data/secret
+proxy_secret_file=/data/proxy_secret
+proxy_multi_file=/data/proxy_multi
+proxy_port="${PORT:-8888}"
+proxy_http_port="${HTTPPORT:-8443}"
+
+if [ ! -f $secret_file ]; then
+    head -c 16 /dev/urandom | xxd -ps |cat > $secret_file
+fi
+
+secret=$(cat $secret_file)
+
+if [ ! -f $proxy_secret_file ]; then
+    curl -s https://core.telegram.org/getProxySecret -o $proxy_secret_file
+    echo "proxy_secret generated: $proxy_secret_file"
+fi
+
+if [ ! -f $proxy_multi_file ]; then
+    curl -s https://core.telegram.org/getProxyConfig -o
+    echo "proxy_multi generated: $proxy_multi_file"
+fi
+
+/usr/bin/mtproto-proxy -u root -p $proxy_port -H $proxy_http_port -S $secret --aes-pwd $proxy_secret_file $proxy_multi_file -M 1
+
