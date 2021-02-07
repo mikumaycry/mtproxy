5e7fad3325e189f2e54522559585f4e1659d8fde
mikumaycry <me@wbuntu.com>
2021-02-07 15:17:58 +0800

    enable fake-tls

--- mtproxy.sh
+++ mtproxy.sh
@@ -7,6 +7,7 @@ proxy_secret_file=/data/proxy_secret
 proxy_multi_file=/data/proxy_multi
 proxy_port="${PORT:-8888}"
 proxy_http_port="${HTTPPORT:-8443}"
+proxy_domain="${DOMAIN:-cloudflare.com}"
 
 if [ ! -f $secret_file ]; then
     head -c 16 /dev/urandom | xxd -ps |cat > $secret_file
@@ -24,5 +25,4 @@ if [ ! -f $proxy_multi_file ]; then
     echo "proxy_multi generated: $proxy_multi_file"
 fi
 
-/usr/bin/mtproto-proxy -u root -p $proxy_port -H $proxy_http_port -S $secret --aes-pwd $proxy_secret_file $proxy_multi_file -M 1
-
+/usr/bin/mtproto-proxy -u root -p $proxy_port -H $proxy_http_port -S $secret --aes-pwd $proxy_secret_file $proxy_multi_file -D $proxy_domain
